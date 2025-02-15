15ca498d3cc91ef2d8d58c38c8840f2b7a707672
Raksha7630 <stharakshya63@gmail.com>
2025-02-15 15:30:04 +0200

    inserting details of the form to the database table

--- HTML AND OTHER PAGES/contact from/contact.php
+++ HTML AND OTHER PAGES/contact from/contact.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect('localhost','root');
+$con=mysqli_connect('localhost','root'); //checking the connection with the database.
 
 if($con){
     echo "Connection Successful"
@@ -7,3 +7,15 @@ if($con){
 
 else {
     echo "Connection Failed";
+}
+
+ mysqli_select_db($con,'puloraca_fusion'); //connecting with the table created in the database
+
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$number = $_POST['Contact number'];
+$message = $_POST['Message'];
+
+$query = "INSERT INTO contact(Name, Email, Contact number, Message) VALUES('$name', '$email', '$number', '$message')";//inserting information of the submitted form to the database table
+
+
